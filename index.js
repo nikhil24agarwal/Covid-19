22c09992331ce3a22538cf9a3231b7f626b39868
unknown <nikhil@yahoo.com>
2020-05-13 09:49:43 +0530

    server setup

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const request=require("request");
 const cheerio=require("cheerio");
+const express=require("express");
+
+const app=express();
+var h=0;
 
 request("https://www.mohfw.gov.in/" , (error,response,html) => {
     if(!error && response.statusCode ==200){
-        console.log(html);
+        const $=cheerio.load(html);
+        var a=$('tbody > tr:nth-child(1) > td:nth-child(3)').text();
+        h=a;
+        console.log(a);
+        
         
     }
+})
+
+app.use("/",function(req,res){
+    res.send(h);
+})
+
+
+
+app.listen(3000,function(){
+    console.log("server is set up");
+    
 })
