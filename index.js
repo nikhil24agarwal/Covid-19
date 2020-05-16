8225aa917e1f5494819eb5f3fb3f55156fea28ab
unknown <nikhil@yahoo.com>
2020-05-16 10:33:09 +0530

    web scraping done at server side

--- index.js
+++ index.js
@@ -6,53 +6,48 @@ const fs = require("fs");
 
 var port=3000;
 const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-var h11=0;
-var h12=0;
-var h13=0;
+app.set("view engine","ejs");       
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+var total=0;
+var active=0;
+var cured=0;
+var death=0;
+var daataa=[0];
 request("https://www.mohfw.gov.in/" , (error,response,html) => {
     if(!error && response.statusCode ==200){
         const $=cheerio.load(html);
-        var a=$('tbody > tr:nth-child(1) > td:nth-child(3)').text();
-        var b=$('tbody > tr:nth-child(1) > td:nth-child(3)').next().text();
-        var c=$('tbody > tr:nth-child(1) > td:nth-child(3)').next().text();
-        h11=a;
-        h12=b;
-        h13=c;
-        console.log(a);
+         total=$('#state-data > div > div > div > div > table > tbody > tr:nth-child(35) > td:nth-child(2) > strong').text();
+        active=$('#site-dashboard > div > div > div > div > ul > li.bg-blue > strong').text();
+         cured=$('#site-dashboard > div > div > div > div > ul > li.bg-green > strong').text();
+        death=$('#site-dashboard > div > div > div > div > ul > li.bg-green > strong').text();
+        for(var i=1;i<34;i++){
+            daataa.push($("#state-data > div > div > div > div > table > tbody > tr:nth-child(" + i + ") > td:nth-child(3)").text());
+            daataa.push($("#state-data > div > div > div > div > table > tbody > tr:nth-child(" + i + ") > td:nth-child(3)").next().text());
+            daataa.push($("#state-data > div > div > div > div > table > tbody > tr:nth-child(" + i + ") > td:nth-child(3)").next().next().text());
+        }
+        
+        console.log(daataa);
+        console.log(daataa.length);
+        
     }
 })
+    //document.querySelector("#state-data > div > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(3)")
+// document.querySelector("#state-data > div > div > div > div > table > tbody > tr:nth-child(33) > td:nth-child(3)")
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
+   
+    res.render('index',{total:total , active:active , cured:cured , death:death});
+    });
+
+app.post("/",function(req,res){
+     var c = req.body.statename;
+     var cc=c.toLowerCase();
+     
+     
+
 })
-
-app.post("/" , function(req,res){
-    res.redirect("/final");
-})
-
-app.get("/final",function(req,res){
-    // $ = cheerio.load(__dirname + "/final.html");
-    // console.log($('body > h1').innerHTML);
-     fs.readFile("./final.html",null,function(error,data){
-         if(error){
-             res.write("error")
-
-         }
-         else{
-             console.log(data);
-             
-
-             
-         }
-     })
-    
-    // res.sendFile(__dirname + "/final.html");
-})
-
-
-
 
 app.listen(port,function(){
     console.log("server is set up");
